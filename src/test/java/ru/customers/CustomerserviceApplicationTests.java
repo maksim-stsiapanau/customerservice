c521f13fe9ea73f2751c0148e1227d1c9d1a060b
Maksim Stepanov <maksim.stsiapanau@gmail.com>
2021-04-29 14:52:59 +0300

    initial commit

---
+++ src/test/java/ru/customers/CustomerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
